193a0fa3f5a43e5a3e3e739f3ff6641094883136
nagamadhun7 <nadakudurunagamadhu@gmail.com>
2025-02-28 20:08:07 -0500

    Guest access

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require("helmet");
 const userRoutes = require("./routes/users");
 const reasonsRoutes = require("./routes/reasons");
 const moodRoutes = require("./routes/mood");
+const guestRoutes = require('./routes/guest')
 const multiModalRoutes = require("./routes/multiModalRoutes"); 
 
 const app = express();
@@ -18,7 +19,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/reasons", reasonsRoutes);
 app.use("/api/mood", moodRoutes);
 app.use("/api/multimodal", multiModalRoutes); 
-app.use("/api/guest/multimodal", multiModalRoutes); 
+app.use("/api/guest/multimodal", guestRoutes); 
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
